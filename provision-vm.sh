41434f555e95fbf2f2738a92d14d7076640ed2fd
Anton Nanchev <ananchev@gmail.com>
2024-12-20 22:46:03 +0100

    update paths

--- provision-vm.sh
+++ provision-vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Refreshing Ignition"
-ignition/butane --pretty --files-dir ignition --strict ignition/vm/config.bu > ignition/vm/config.ign
+ignition/butane --pretty --files-dir ignition/vm --strict ignition/vm/config.bu > ignition/vm/config.ign
 
 echo "Instantiating the QEMU VM"
-cp qemu/fedora-coreos-40.20240920.3.0-qemu.aarch64.qcow2 qemu/fedora-coreos.qcow2
+cp qemu/fedora-coreos-41.20241122.3.0-qemu.aarch64.qcow2 qemu/fedora-coreos.qcow2
 # qemu/launch-with-screen.sh
 screen -dmS run-coreos-qemu qemu/launch.sh
 echo "Sleeping 30 seconds to let the VMs boot"
--- qemu/launch-with-screen.sh
+++ qemu/launch-with-screen.sh
@@ -10,7 +10,7 @@ sudo /opt/homebrew/bin/qemu-system-aarch64 \
   -m 4096 \
   -drive file=qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
   -drive file=qemu/edk2-arm-vars.fd,if=pflash,format=raw \
-  -fw_cfg name=opt/com.coreos/config,file=ignition/config.ign \
+  -fw_cfg name=opt/com.coreos/config,file=ignition/vm/config.ign \
   -drive file=qemu/fedora-coreos.qcow2 \
   -device qemu-xhci \
   -netdev vmnet-bridged,id=n1,ifname=en0 \
